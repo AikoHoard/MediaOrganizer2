aa66110e447825b3c7281ffa860fd17bd6cd4827
AikoHoard <157399681+AikoHoard@users.noreply.github.com>
2024-08-28 10:04:08 -0600

    Add project files.

---
+++ MediaOrganizer/MediaListControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MediaOrganizer
+{
+    /// <summary>
+    /// Interaction logic for MediaListControl.xaml
+    /// </summary>
+    public partial class MediaListControl : UserControl
+    {
+        public ObservableCollection<MediaItem> MediaItems { get; set; }
+        public string MediaType { get; set; }
+
+        public MediaListControl()
+        {
+            InitializeComponent();
+            MediaItems = new ObservableCollection<MediaItem>();
+            DataContext = this;
+
+            if (MediaType == "Video")
+                MediaItems.Add(new MediaItem { Name = "Video.mp4" });
+            else if (MediaType == "Music")
+                MediaItems.Add(new MediaItem { Name = "Music.mp3" });
+            else if (MediaType == "Web")
+                MediaItems.Add(new MediaItem { Name = "Website1" });
+        }
+    }
+    public class MediaItem
+    {
+        public string Name { get; set; }
+    }
+}
